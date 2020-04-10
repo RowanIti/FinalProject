cccf4194e18ecfd0c73b91ec6a7eda871081041a
RowanIti <63440469+RowanIti@users.noreply.github.com>
2020-04-10 17:28:18 +1200

    adding files

---
+++ FinalProject/FinalProject/Controllers/InsuranceController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        // GET: Insurance
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
